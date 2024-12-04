2282f159e72f778138911c946d277f780d8ecdd6
sabriwnaa <sabrinahm2006@gmail.com>
2024-12-04 08:38:24 -0300

    style

---
+++ HeaderFooter/header.php
@@ -0,0 +1,19 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    
+            <a href="index.php">IFindArt</a>
+       
+    
+</header>
+
+</body>
+</html>
