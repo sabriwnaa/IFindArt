632d20058151d709feafc21daa535075cb3a53b0
Gabriel Klafke Teixeira <164067823+GabrielKKT@users.noreply.github.com>
2024-12-04 08:40:47 -0300

    .

---
+++ restrita.php
@@ -0,0 +1,23 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ifindart</title>
+</head>
+<body>
+<div class='container'>
+    <?php include 'header.php'; ?>
+
+    <div class='main'>
+        <a href=""></a>
+    </div>
+
+    <?php include 'footer.php'; ?>
+
+    </div>
+</body>
+</html>
