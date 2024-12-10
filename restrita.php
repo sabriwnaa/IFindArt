3f519e7a7ad9e024fef61c82ead2e8b1afaca398
AnaAriel <149743790+AnaAriel@users.noreply.github.com>
2024-12-10 19:10:13 -0300

    listagem restrita

--- restrita.php
+++ restrita.php
@@ -30,52 +30,58 @@ if ($modo === 'votacao') {
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IFindArt</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<div class="container">
-    <?php include 'HeaderFooter/header.php'; ?>
+    <div class="container">
+        <?php include 'HeaderFooter/header.php'; ?>
 
 
-    
 
-    <div class='LC'>
-        <div class='itemLC' style='background-color:#f8ddb0; color:#B02E0C;'>    
-    <a href="?mode=votacao">Votação</a>
+
+        <div class='LC'>
+            <div class='itemLC' style='background-color:#f8ddb0; color:#B02E0C;'>
+                <a href="?mode=votacao">Votação</a>
+            </div>
+            <div class='itemLC' style='background-color:#EB4511'>
+                <a href="?mode=ranking">Ranking</a>
+            </div>
         </div>
-        <div class ='itemLC' style='background-color:#EB4511'>
-            <a href="?mode=ranking" >Ranking</a>
-        </div>
-    </div>
 
-    <div class="main">
-        <?php if ($modo === 'votacao'): ?>
-            <div class="containerVotacao">
-            <?php if ($itemAleatorio): ?>
-                <h2>Vote neste item:</h2>
-                <img src="images/<?= htmlspecialchars($itemAleatorio['imagem']); ?>" alt="Imagem">
-                <form method="POST" action="votar.php">
-                    <input type="hidden" name="item_id" value="<?= $itemAleatorio['idItem']; ?>">
-                    <button type="submit" name="voto" value="1">Like</button>
-                    <button type="submit" name="voto" value="0">Dislike</button>
-                    <button type="submit" name="voto" value="2">Pular</button>
-                </form></div>
-            <?php else: ?>
-                <p>Nenhum item disponível para votação.</p>
-            <?php endif; ?>
-        <?php elseif ($modo === 'ranking'): ?>
-            <div class="containerRanking">
-            <h1>Artistas favoritos dos estudantes</h1>
-            
-                <?php if (!empty($ranking)){
-                    $posicao = 0;
-                            foreach ($ranking as $item) {
-                                $posicao++;
-                                echo "
+        <div class="main">
+            <?php if ($modo === 'votacao'): ?>
+                <div class="containerVotacao">
+                    <?php if ($itemAleatorio): ?>
+                        <div class="votacaoFoto" style="background-image: url('images/<?= htmlspecialchars($itemAleatorio['imagem']); ?>');">
+                            <div class="barraArtista">
+                                <h2><?= htmlspecialchars($itemAleatorio['titulo']); ?></h2>
+                            </div>
+                            <form class="botoesVotacao" method="POST" action="votar.php">
+                                <input type="hidden" name="item_id" value="<?= $itemAleatorio['idItem']; ?>">
+                                <button type="submit" name="voto" value="1" class="botao">Like</button>
+                                <button type="submit" name="voto" value="0" class="botao">Dislike</button>
+                                <button type="submit" name="voto" value="2" class="botao">Pular</button>
+                            </form>
+                        </div>
+                    <?php else: ?>
+                        <p>Nenhum item disponível para votação.</p>
+                    <?php endif; ?>
+                </div>
+            <?php elseif ($modo === 'ranking'): ?>
+                <div class="containerRanking">
+                    <h1>Artistas favoritos dos estudantes</h1>
+
+                    <?php if (!empty($ranking)) {
+                        $posicao = 0;
+                        foreach ($ranking as $item) {
+                            $posicao++;
+                            echo "
                                 <div class='item' " . ($posicao === 1 ? "style='border-top: 1px solid rgb(255, 255, 255);'" : "") . ">
                                     <div class='posicao'>
                                         <h1>{$posicao}°</h1>
@@ -90,15 +96,16 @@ if ($modo === 'votacao') {
                                         </div>
                                     </div>
                                 </div>";
-                            }
-                        } else {
-                            echo "<p>Nenhum item foi votado ainda.</p>";
                         }
-                        ?>
-                    </div>
+                    } else {
+                        echo "<p>Nenhum item foi votado ainda.</p>";
+                    }
+                    ?>
+                </div>
         </div>
-        <?php endif; ?>
+    <?php endif; ?>
     </div>
-</div>
+    </div>
 </body>
+
 </html>
