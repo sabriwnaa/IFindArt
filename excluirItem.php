efd8144c15254a2675cf477479531a30b8d72d0a
Diogo Vettorazzi <149743790+AnaAriel@users.noreply.github.com>
2024-12-10 17:22:07 -0300

    editar e excluir

---
+++ excluirItem.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require_once "vendor/autoload.php";
+
+if (!isset($_SESSION['adminLogado']) || $_SESSION['adminLogado'] !== true) {
+    header("Location: index.php");
+    exit();
+}
+
+$idItem = $_GET['id'];
+$item = Item::findById($idItem);
+
+if ($item && $item->delete()) {
+    header("Location: restritaAdmin.php");
+    exit();
+} else {
+    echo "Erro ao excluir o item.";
+}
+?>
